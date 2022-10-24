f221e9e60336a3cbed2f40b88f0a74290bc677f9
Pedro Soares <34283375+pppedro173@users.noreply.github.com>
2022-10-25 00:47:46 +0100

    Update reverse-integer.java

--- reverse-integer.java
+++ reverse-integer.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int reverse(int x) {
-        if(x >= 1_000_000_000 && (x%1_000_000_00) < 10 && (x%1_000_000_00) > 0) return 0;
-        if(x <= -1_000_000_000 && (x%1_000_000_00) > -10 && (x%1_000_000_00) < 0) return 0;
+ public static int reverse(int x) {
+        if(x == -2147483648) return 0;
         int n;
         boolean signal = false;
         if(x < 0){
             signal =true;
-            x = x*-1;
+            x = Math.abs(x);
+            System.out.println(x);
         }
         StringBuilder s = new StringBuilder();
         while(x >= 10){
@@ -15,7 +15,14 @@ class Solution {
             s.append(Integer.toString(n));
         }
         s.append(Integer.toString(x));
-        n =Integer.parseInt(s.toString());
+
+        try {
+            n =Integer.parseInt(s.toString());
+        }
+        catch(Exception e) {
+            return 0;
+        }
+
         if(signal) n = n * (-1);
 
         return n;
