bf00d0a8f572b18998f0eb8901296f05c727f3e3
Pedro Soares <34283375+pppedro173@users.noreply.github.com>
2022-10-25 18:31:40 +0100

    Update search-insert-position.java

--- search-insert-position.java
+++ search-insert-position.java
@@ -1,8 +1,19 @@
+class Solution {
     public int searchInsert(int[] nums, int target) {
-        int i;
-        for(i  = 0; i < nums.length; i++){
-            if(nums[i] > target) return i;
-            if(nums[i] == target) return i;
+        int s  = 0;
+        int e = nums.length - 1;
+        int m = 0;
+        
+        while( s <= e){
+            m = (s+e)/2;
+            if(nums[m] ==  target) return m;
+
+            if(nums[m] > target){
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
         }
-        return i;
+        return s;
     }
+}
