06c71367fde3d1404d9f67da12c3d0329a0ff26f
Pedro Soares <34283375+pppedro173@users.noreply.github.com>
2022-10-28 22:58:31 +0100

    Update reverse-string.java

--- reverse-string.java
+++ reverse-string.java
@@ -1,10 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        char aux;
-        for(int i = 0; i < (s.length/2); i++){
-            aux = s[i];
-            s[i] = s[s.length - i - 1];
-            s[s.length - i - 1] = aux;
+        int st = 0;
+        int e = s.length - 1;
+        
+        while(st < e){
+            char p = s[st];
+            s[st] = s[e];
+            s[e] = p;            
+            st +=1;
+            e -=1;
         }
     }
 }
